19f0cb2f67beee35cbb5c593a8899c2ac00daa91
Krish <89772346+krishpamecha@users.noreply.github.com>
2023-06-28 19:19:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+Harry_potter_theme_song="";
+
+function setup(){
+    canvas = createCanvas(600,530);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function preload(){
+    Harry_potter_theme_song = loadSound("song.mp3");
+}
+
+function draw(){
+    image(video,0,0,600,530);
+}
